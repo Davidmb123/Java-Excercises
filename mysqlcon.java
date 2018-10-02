3fe2b6dcbae2e6ae692c9635d565ba5edf1973dd
Davidmb123 <35699126+Davidmb123@users.noreply.github.com>
2018-10-01 17:19:28 -0700

    Add files via upload

--- mysqlcon.java
+++ mysqlcon.java
@@ -1,32 +1,49 @@
 import java.sql.*;
-public class mysqlcon {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		try{  
-			Class.forName("com.mysql.jdbc.Driver");  
-			String user="root";
-			String pass="";
-			String url="jdbc:mysql://localhost:3306";
-			String database="alumnos";
-			Connection con=DriverManager.getConnection( "jdbc:mysql://127.0.0.1:3306/test","root","");
-		PreparedStatement pst=(PreparedStatement) con.prepareStatement("Select * from alumnos");
-			ResultSet rs=(ResultSet) pst.executeQuery();
-		if(rs.next()) {  
-		System.out.println("Si se logro la conexcion");
-		}
-		
-		else {
-			
-			System.out.println("No se logro la conexion");
-		}}
-			
-	catch(Exception e)
+public class mysqlcon {
+	// TODO Auto-generated method stub
+	public static void main(String[] args) throws Throwable  {
+
+		{
 			{
-				System.out.println(e);
+				Class.forName("com.mysql.jdbc.Driver");
+				String user = "root";
+				String pass = "";
+				String url = "jdbc:mysql://localhost:3306";
+				String database = "test";
+				Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+
+				try {
 				
-			}  
-			  
+					PreparedStatement pst = (PreparedStatement) con.prepareStatement("Select * from alumnos");
+					ResultSet rs = (ResultSet) pst.executeQuery();
+					if (rs.next()) {
+						System.out.println("Si se logro la conexcion");
+
+					}
+
+					else {
+
+						System.out.println("No se logro la conexion");
+					}
+				}
+
+				catch (Exception e) {
+					System.out.println(e);
+
+				}
+				String query="Insert into alumnos values ('Carlos Sandez','e009678',18)";
+				try {
+					PreparedStatement statement= con.prepareStatement(query);
+					statement.execute();
+					System.out.println("Insercion con exito");
+					
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+				
+				
+			}
 		}
 	}
+}
